9e07102078a97d007e8498edeaca5656d8f8251b
Ghadeer Abd Alhameed <61324743+Gh-abd-alhameed@users.noreply.github.com>
2022-02-27 17:03:09 +0200

    Add files via upload

--- inc/assets/js/main.js
+++ inc/assets/js/main.js
@@ -1,29 +1,29 @@
+// logo site api 
 document.onclick = (e) => {
   if (e.target.id === "remove_logo_site") {
-    const url = e.target.getAttribute("data-url");
-    const image_name = e.target.getAttribute("data-image");
-
+    let url = e.target.getAttribute("data-url");
+    let image_name = e.target.getAttribute("data-image");
+    console.log(image_name);
     remove_logo(url, image_name);
   }
 };
+
+let msg_delete = document.getElementById('msg-delete');
+let msg_error = document.getElementById('msg-error');
 async function remove_logo(api_url, imagename = "") {
-  let response = await fetch(api_url, {
+  let formdata = new URLSearchParams(new FormData());
+  formdata.append("action", "maxart_remove_logo");
+  formdata.append("imageremove", imagename);
+  const response = await fetch(api_url, {
     method: "POST",
-    headers: {
-      "Content-Type": "text/html"
-    },
-    body: new URLSearchParams({
-      image_remove :  imagename,
-    }) ,
+    body: formdata,
   });
-  if(response.status === 200){
-    let data = response;
-    console.log(data) ;
+  if (response.status === 200) {
+    msg_delete.style.display = "block";
+  }else{
+    msg_error.style.display = "block";
   }
 }
 
-// const response = new XMLHttpRequest();
-// response.open("POST",url,true) ;
-// console.log(response);
-// console.log("ok");
-// response.send("?action=remove_logo&imagename=image_name");
+
+
