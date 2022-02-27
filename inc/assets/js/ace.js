410358355e356b2cc8ba2ba2f72f303e3c3f705c
Ghadeer Abd Alhameed <61324743+Gh-abd-alhameed@users.noreply.github.com>
2022-02-27 18:05:00 +0200

    Add files via upload

--- inc/assets/js/ace.js
+++ inc/assets/js/ace.js
@@ -1,4 +1,14 @@
 // ace editor 
+let textarea = document.getElementById("maxart_register_custom_css");
 var editor = ace.edit("editor");
 editor.setTheme("ace/theme/monokai");
-editor.session.setMode("ace/mode/javascript");
+editor.session.setMode("ace/mode/css");
+let form = document.getElementById('form-css');
+
+form.onsubmit = ()=>{
+    textarea.innerHTML = editor.getSession.getValue();
+}
+
+
+
+
