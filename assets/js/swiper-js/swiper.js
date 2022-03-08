9e8394ba1f9b0e61c460779e5659b94282bc1213
unknown <gh.abd.alhameed.97@gmail.com>
2022-03-08 17:11:17 +0200

    add related in single page product

--- assets/js/swiper-js/swiper.js
+++ assets/js/swiper-js/swiper.js
@@ -21,19 +21,14 @@ var swiper = new Swiper(".format-gallery", {
 });
 
 var swiper = new Swiper(".related_product", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: "auto",
-  coverflowEffect: {
-    rotate: 50,
-    stretch: 0,
-    depth: 100,
-    modifier: 1,
-    slideShadows: true,
-  },
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
   },
   navigation: {
     nextEl: ".swiper-button-next",
